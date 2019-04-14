3f659f3f23dfb5039cf5fd5aa3b4a43dfa2d5cb4
Milad D Z <m.dastanzand@hotmail.com>
2019-04-14 11:35:54 +0430

    Delete Some Comment

--- DfsPathFinder/Program.cs
+++ DfsPathFinder/Program.cs
@@ -11,9 +11,6 @@ namespace PathFinder
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
