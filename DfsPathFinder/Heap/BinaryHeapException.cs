b7e4a3d5f1f108dc46a2853809ab79a475fa051a
Milad Dastan Zand <Dastanzand.M@orchidpharmed.com>
2019-04-14 11:24:12 +0430

    Add project files.

---
+++ DfsPathFinder/Heap/BinaryHeapException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathFinder.Heap
+{
+    class BinaryHeapException : Exception
+    {
+        /* Private: */
+	    //private static long serialVersionUID = 1L;
+
+        /* Public: */
+	    public BinaryHeapException(String message):base(message)
+        {
+	    }
+    }
+}
